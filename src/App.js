017aea4fbb8a6e8a0d846dd144d2fa45e538876f
Your Name <you@example.com>
2025-05-10 09:53:01 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  let email = 'fracz@agh.edu.pl';
+
+  function handleChange(event) {
+    console.log(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>System do zapisów na zajęcia</h1>
+        <h2>Twój e-mail to {email}.</h2>
+        <input type="text" onChange={handleChange}/>
+      </div>
   );
 }
 
